a0123efc0a99700fc4702851dcf12f50483e777f
Arnav <arnavsolanki511@gmail.com>
2024-01-16 18:00:09 +0530

    Only Activity Panel Left

--- src/ChatListHistory/ChatListHistory.js
+++ src/ChatListHistory/ChatListHistory.js
@@ -14,11 +14,33 @@ function ChatListHistory() {
 
     const [open, setOpen] = useState(false)
 
+    const [deleteMenu, setdeleteMenu] = useState(false)
+    const deletebttnRef = useRef(null)
+
     const [currChat, setCurrChat] = useState('');
 
     const [chats, setChats] = useState([])
 
     const [shouldNavigate, setShouldNavigate] = useState(false);
+
+    const [titleToDelete, setTitleToDelete] = useState('');
+
+    const onDeleteBttnPress = (e) => {
+        const t = e.currentTarget.id.slice(1);
+        setTitleToDelete(t);
+        setdeleteMenu(true);
+    }
+
+    const DeleteChat = (e) => {
+        e.preventDefault();
+        const chatToDelete = chats.find(chat => chat.id === titleToDelete);
+        const index = chats.indexOf(chatToDelete);
+        chats.splice(index, 1);
+        setChats(chats);
+        localStorage.setItem('chats', JSON.stringify(chats));
+        setEditMode(false);
+        setdeleteMenu(false);
+    }
 
     useEffect(() => {
         const chats = JSON.parse(localStorage.getItem('chats'))
@@ -117,7 +139,7 @@ function ChatListHistory() {
                                 </button>
                             </Link>
                             {editMode && (
-                                <button className='flex justify-between gap-x-6 m-3 p-5 bg-red-600 items-center rounded-2xl'>
+                                <button id={`DB${chat.title}`} className='flex justify-between gap-x-6 m-3 p-5 bg-red-600 items-center rounded-2xl hover:bg-red-500' onClick={onDeleteBttnPress}>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-6 h-6">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                     </svg>
@@ -214,6 +236,79 @@ function ChatListHistory() {
                                             className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
                                             onClick={() => setOpen(false)}
                                             ref={cancelButtonRef}
+                                        >
+                                            Cancel
+                                        </button>
+                                    </div>
+                                </Dialog.Panel>
+                            </Transition.Child>
+                        </div>
+                    </div>
+                </Dialog>
+            </Transition.Root>
+
+            {/* Delete Chat Popup Menu */}
+            <Transition.Root show={deleteMenu} as={Fragment}>
+                <Dialog as="div" className="relative z-10" initialFocus={deletebttnRef} onClose={setdeleteMenu}>
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                    </Transition.Child>
+
+                    <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                        <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            >
+                                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:max-w-lg w-80">
+                                    <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                                        <div className="flex flex-col items-center">
+                                            <img
+                                                className="h-40 w-40 rounded-full"
+                                                src={ReXLogo}
+                                                alt=""
+                                            />
+                                            <div className="mt-8 text-center">
+                                                <Dialog.Title as="h1" className="text-2xl font-semibold leading-6 text-gray-900">
+                                                    Delete Chat
+                                                </Dialog.Title>
+                                                <div className="mt-2">
+                                                    <p className="text-sm text-gray-500">
+                                                        Are you sure you want to delete this chat?
+                                                    </p>
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="bg-gray-50 px-4 py-3 flex flex-col sm:px-6">
+                                        <Link to='/chat'>
+                                            <button
+                                                type="button"
+                                                className="inline-flex w-full justify-center rounded-full bg-indigo-600 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 disabled:bg-gray-300 disabled:cursor-not-allowed p-4 m-1"
+                                                onClick={DeleteChat}
+                                            >
+                                                Yes, Delete
+                                            </button>
+                                        </Link>
+                                        <button
+                                            type="button"
+                                            className="mt-3 inline-flex w-full justify-center rounded-full bg-indigo-100 text-sm font-semibold text-indigo-600 shadow-sm ring-1 ring-inset ring-indigo-100 hover:bg-indigo-50 p-4 m-1"
+                                            onClick={() => setdeleteMenu(false)}
+                                            ref={deletebttnRef}
                                         >
                                             Cancel
                                         </button>
